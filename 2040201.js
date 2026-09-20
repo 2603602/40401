81e70f645ed5f5dd965e2bca35a10c4f435f679a
2603602 <yz861221-assistance@inboxted.mail.pl>
2026-09-21 00:34:46 +0200

    005

--- 2040201.js
+++ 2040201.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Amazon → Google Sheets
 // @namespace    local.amazon.sheet
-// @version      1.4.1
+// @version      1.4.11
 // @description  Cross-browser Amazon → Google Sheets collector with self-update and local Apps Script configuration
 // @match        https://www.amazon.com/*
 // @grant        GM_xmlhttpRequest
